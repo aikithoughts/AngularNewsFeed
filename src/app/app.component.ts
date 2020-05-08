f2b4ebc54effc21b279174b77e4fd9eee2658f24
David Shevitz 🔵 <dshevitz@google.com>
2020-05-08 14:47:10 +0000

    add new files to initial application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { NEWSITEMS } from './mock-newsitems';
+import { NewsItem } from './news-item';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularNews';
+
+  // newsItem: NewsItem = {
+  //   url: 'https://angular.io/docs',
+  //   title: 'Introduction to the Angular Docs',
+  //   publishedDate: 'January 1, 2015',
+  //   lastUpdatedDate: 'May 8, 2020',
+  //   author: {
+  //     name: 'Angular Team',
+  //     website: 'https://angular.io'
+  //   }
+  // };
+
+  newsItems = NEWSITEMS;
+
 }
